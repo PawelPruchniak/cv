d443eb2291232b35895d6f50084fe1fca96be612
PawelPruchniak <pw.pruchniak@gmail.com>
2023-03-27 17:33:38 +0200

    Add dynamic experience duration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { ViewportScroller } from '@angular/common';
+import * as moment from 'moment';
+import { Moment } from 'moment';
 
 @Component({
   selector: 'cv-root',
@@ -7,11 +9,27 @@ import { ViewportScroller } from '@angular/common';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'savings-io-frontend';
-
   constructor(private viewportScroller: ViewportScroller) {}
 
   onClickScroll(elementId: string) {
     this.viewportScroller.scrollToAnchor(elementId);
+  }
+  get getJavaDuration(): string {
+    return this.getTranslatedDuration(moment('2021-04-01'));
+  }
+
+  get getTypescriptDuration(): string {
+    return this.getTranslatedDuration(moment('2022-06-01'));
+  }
+
+  private getTranslatedDuration(start: Moment): string {
+    const duration = moment.duration(moment().diff(start));
+    const years = Math.floor(duration.asMonths() / 12);
+    const months = Math.floor(duration.asMonths() % 12);
+
+    let result = '';
+    if (years > 0) result += years + ' yr';
+    if (months > 0) result += ' ' + months + ' mos';
+    return result;
   }
 }
