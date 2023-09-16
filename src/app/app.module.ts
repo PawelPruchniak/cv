a47ed732cac24c4f44cb736701d1ca4ca7afa071
PawelPruchniak <pw.pruchniak@gmail.com>
2023-09-16 23:48:49 +0200

    Cleanup libraries

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
-import { MatMenuModule } from '@angular/material/menu';
 import { ScrollingModule } from '@angular/cdk/scrolling';
 import { NgOptimizedImage } from '@angular/common';
 import { MatLineModule } from '@angular/material/core';
@@ -21,17 +17,13 @@ import { MatLineModule } from '@angular/material/core';
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpClientModule,
     BrowserAnimationsModule,
     MatCardModule,
-    MatProgressSpinnerModule,
     MatDividerModule,
     MatIconModule,
     MatToolbarModule,
     MatButtonModule,
     MatListModule,
-    MatMenuModule,
     ScrollingModule,
     NgOptimizedImage,
     MatLineModule,
